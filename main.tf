15209397f5c351906b3e0f161feeb9a1ecc9d0fd
Arthur <immola.chr.arthur@dxc.com>
2020-04-06 01:17:25 +0530

    33 commit

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@ terraform {
 	backend "s3" {
 		bucket = "terraform-bucket-new"
 		key = "terraform.tfstate"
-		region = ["${var.region}"]
+		region = "var.region"
 	}
 }
 // use AWS terraform provider
 provider "aws" {
-	region = "${var.region}"  
+	region = "var.region"
 }
 
 // Create EC2 Instance
 resource "aws_instance" "default" {
-	ami	= ["${var.ami}"]
-	count = ["${var.count}"]
+	ami	= "var.ami"
+	count = 2
 	key_name = "Terraform"
 	vpc_security_group_ids = ["${aws_security_group.default.id}"]
-	instance_type = ["${var.instance_type}"]
+	instance_type = "var.instance_type"
 	tags = {
 	Name = "Terraform-default"  
 }
